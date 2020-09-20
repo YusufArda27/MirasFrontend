fabc3f89c6b77fcbc2cc522b201e05002ea52300
yusard <y.ardavardar@gmail.com>
2020-09-20 10:43:43 +0300

    added a basic homepage

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+const Index = () =>(
+    <div  className="flex-container">
+        <h1 id="title">Miras</h1>
+        <div className="flex-container">
+            <p id="descrip">Lorem ipsum dolor sit amet</p>
+        </div>
+    
+    </div>
+
+
+);
+
+export default Index;
